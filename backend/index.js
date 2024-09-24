29820ad8a0210b2731ae61812424234b116f672f
Hamza <Hamzabashir02001@gmail.com>
2024-09-24 13:40:09 +0500

    cors

--- backend/index.js
+++ backend/index.js
@@ -58,7 +58,14 @@ const connectDB = async () => {
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: true }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://putko-main.vercel.app');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 
 // Routes
 app.use('/api/auth', authRoutes);
