992491c0e0e6e32621f4362c517b1260916383fb
Hamza <Hamzabashir02001@gmail.com>
2024-09-24 15:37:40 +0500

    verc

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -7,14 +7,7 @@ const nextConfig = {
     NEXT_PUBLIC_UPLOAD_PRESET: process.env.NEXT_PUBLIC_UPLOAD_PRESET,
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://putko-backend.vercel.app/api/:path*',
-      },
-    ]
-  },
+  
 };
 
 export default nextConfig;
