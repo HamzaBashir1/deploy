7a66ae07ee1a8bbb7d35d2b4659182044128a916
Hamza <Hamzabashir02001@gmail.com>
2024-10-16 20:13:33 +0500

    propertcard aligned center

--- frontend/src/app/components/CardSection.js
+++ frontend/src/app/components/CardSection.js
@@ -3,14 +3,19 @@ import PropertyCard from './PropertyCard';
 
 const CardSection = () => {
   return (
-      <div className='mx-4 sm:mx-20 mt-8'>
-        <div className='flex flex-nowrap lg:flex-wrap lg:gap-6 overflow-x-auto lg:overflow-x-visible snap-x snap-mandatory scrollbar-hide'>
-          {/* Feature Cards */}
-          <div className='min-w-full lg:min-w-[calc(24%-0.75rem)] snap-start'>
+    <div className="mx-4 sm:mx-20 mt-8">
+      <div className="flex flex-nowrap lg:flex-wrap gap-4 lg:gap-6 overflow-x-auto lg:overflow-x-visible snap-x snap-mandatory scrollbar-hide">
+        {/* Mobile layout: Display PropertyCards in a single row and center them */}
+        <div className="flex justify-center items-center min-w-full sm:min-w-[60%] md:min-w-[40%] lg:min-w-[10%] xl:min-w-[calc(20%-1.5rem)] snap-start sm:hidden">
           <PropertyCard />
-          </div>
+        </div>
+
+        {/* Desktop layout: Use this section for the original design */}
+        <div className="hidden sm:flex gap-4">
+          <PropertyCard />
         </div>
       </div>
+    </div>
   );
 };
 
