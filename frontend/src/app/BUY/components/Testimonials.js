eb18152219824d21c182c439ee738862e1d55192
Hamza <Hamzabashir02001@gmail.com>
2024-10-25 16:29:42 +0500

    fix testimonial in BUY

--- frontend/src/app/BUY/components/Testimonials.js
+++ frontend/src/app/BUY/components/Testimonials.js
@@ -1,14 +1,14 @@
 "use client";
-import React ,{ useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const Testimonials = () => {
   const TestimonialCard = ({ image, title, location, description, name, role }) => (
-    <div className="flex-shrink-0 w-full overflow-hidden text-white bg-gray-900 rounded-lg shadow-lg px-14 lg:px-0 sm:w-80">
-      <img src={image} alt={title} className="object-cover w-full h-48 " />
-      <div className="p-6 ">
+    <div className="flex-shrink-0 w-full text-white bg-gray-900 rounded-lg shadow-lg p-6 sm:w-80">
+      <img src={image} alt={title} className="object-cover w-full h-48 rounded-t-lg" />
+      <div className="p-6">
         <h3 className="mb-2 text-lg font-semibold">{title}</h3>
         <p className="mb-4 text-sm text-gray-400">{location}</p>
         <p className="mb-6 text-sm">{description}</p>
@@ -24,17 +24,13 @@ const Testimonials = () => {
       </div>
     </div>
   );
-  const [slidesToShow, setSlidesToShow] = useState(1);
-  
+
   const sliderRef = useRef(null);
-  //for applying to 4 images on large and 1 on small
+  const [slidesToShow, setSlidesToShow] = useState(1);
+
   useEffect(() => {
     const updateSlidesToShow = () => {
-      if (window.innerWidth >= 768) {
-        setSlidesToShow(4);
-      } else {
-        setSlidesToShow(1);
-      }
+      setSlidesToShow(window.innerWidth >= 768 ? 4 : 1);
     };
 
     window.addEventListener("resize", updateSlidesToShow);
@@ -44,52 +40,43 @@ const Testimonials = () => {
       window.removeEventListener("resize", updateSlidesToShow);
     };
   }, []);
+
   const handlePrevious = () => {
-    console.log("4234ff");
-    if (sliderRef.current) {
-      sliderRef.current.slickPrev();
-    }
+    if (sliderRef.current) sliderRef.current.slickPrev();
   };
 
   const handleNext = () => {
-    console.log("4234");
-    if (sliderRef.current) {
-      sliderRef.current.slickNext();
-    }
+    if (sliderRef.current) sliderRef.current.slickNext();
   };
 
   // Slider settings
   const settings = {
-    dots: false,
+    dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: slidesToShow,
-    slidesToScroll: 2,
-    arrows: true,
+    slidesToShow,
+    slidesToScroll: 1,
+    arrows: false,  // Hide arrows within the slider itself
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: slidesToShow,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: false,
+          slidesToShow: 3,
+          slidesToScroll: 1,
         }
       },
       {
         breakpoint: 768,
         settings: {
-          slidesToShow: slidesToShow,
+          slidesToShow: 2,
           slidesToScroll: 1,
-          dots: true,
         }
       },
       {
         breakpoint: 640,
         settings: {
-          slidesToShow: slidesToShow,
+          slidesToShow: 1,
           slidesToScroll: 1,
-          dots: true,
         }
       }
     ]
@@ -98,14 +85,13 @@ const Testimonials = () => {
   return (
     <div className="px-6 py-12 bg-gray-900">
       <h2 className="mb-8 text-3xl font-bold text-center text-white">They said about us</h2>
-      <div className="relative mySwiper" >
-      {/* Left arrow */}
-      <button onClick={handlePrevious} className="absolute left-0 z-10 items-center justify-center hidden w-10 h-10 -ml-5 text-white bg-gray-700 rounded-full mySwiper top-52 lg:flex">
-        &#8592;
-      </button>
-      
-        <Slider {...settings} className="" >
-        <div className=''>
+      <div className="relative">
+        {/* Left Arrow */}
+        <button onClick={handlePrevious} className="absolute left-0 z-10 items-center justify-center w-10 h-10 -ml-5 text-white bg-gray-700 rounded-full top-1/2 transform -translate-y-1/2">
+          &#8592;
+        </button>
+
+        <Slider {...settings} ref={sliderRef}>
           <TestimonialCard
             image="/Pieniny.png"
             title="Bungalow in the heart of Liptov"
@@ -113,11 +99,7 @@ const Testimonials = () => {
             description="Despite the fact that Fiemso is a new accommodation portal, compared to competing websites on our market, it is far ahead of them."
             name="Gorazd"
             role="Owner"
-            
           />
-          </div>
-          {/* Add more TestimonialCards as needed */}
-          <div className=''>
           <TestimonialCard
             image="/Pieniny.png"
             title="Charming Cottage"
@@ -125,11 +107,15 @@ const Testimonials = () => {
             description="The interface is user-friendly and makes managing bookings effortless."
             name="Anna"
             role="Manager"
-            
-            
           />
-          </div>
-          <div className=''>
+          <TestimonialCard
+            image="/Pieniny.png"
+            title="Bungalow in the heart of Liptov"
+            location="Bungalow, Liptovská Mara, Slovakia"
+            description="Despite the fact that Fiemso is a new accommodation portal, compared to competing websites on our market, it is far ahead of them."
+            name="Gorazd"
+            role="Owner"
+          />
           <TestimonialCard
             image="/Pieniny.png"
             title="Charming Cottage"
@@ -137,11 +123,7 @@ const Testimonials = () => {
             description="The interface is user-friendly and makes managing bookings effortless."
             name="Anna"
             role="Manager"
-            
-            
           />
-          </div>
-          <div className=''>
           <TestimonialCard
             image="/Pieniny.png"
             title="Charming Cottage"
@@ -149,30 +131,17 @@ const Testimonials = () => {
             description="The interface is user-friendly and makes managing bookings effortless."
             name="Anna"
             role="Manager"
-            
-            
           />
-          </div>
-          <div className=''>
-          <TestimonialCard
-            image="/Pieniny.png"
-            title="Charming Cottage"
-            location="Cottage, High Tatras, Slovakia"
-            description="The interface is user-friendly and makes managing bookings effortless."
-            name="Anna"
-            role="Manager"
-            
-            
-          />
-          </div>
+          {/* Additional TestimonialCards */}
         </Slider>
-         {/* Right arrow */}
-      <button  onClick={handleNext} className="absolute right-0 z-10 items-center justify-center hidden w-10 h-10 -mr-5 text-white bg-gray-700 rounded-full mySwiper top-52 lg:flex">
-      &#8594;
-    </button>
+
+        {/* Right Arrow */}
+        <button onClick={handleNext} className="absolute right-0 z-10 items-center justify-center w-10 h-10 -mr-5 text-white bg-gray-700 rounded-full top-1/2 transform -translate-y-1/2">
+          &#8594;
+        </button>
       </div>
     </div>
   );
-}
+};
 
 export default Testimonials;
