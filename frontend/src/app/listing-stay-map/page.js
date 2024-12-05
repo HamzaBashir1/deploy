1d214a848d54fcbe6d5b576c5a3bcefe1dc526ed
Hamza <Hamzabashir02001@gmail.com>
2024-12-05 19:53:51 +0500

    fix the deploy error

--- frontend/src/app/listing-stay-map/page.js
+++ frontend/src/app/listing-stay-map/page.js
@@ -1,4 +1,6 @@
-import React from "react";
+"use client"
+
+import React, { Suspense } from "react";
 import SectionGridHasMap from "./component/SectionGridHasMap";
 import HeroSection from "./component/HeroSection";
 import BgGlassmorphism from "../components/BgGlassmorphism";
@@ -6,6 +8,7 @@ import Subscribe from "../components/Subscribe";
 import BackgroundSection from "../components/BackgroundSection";
 import GridAuthorBox from "../components/GridAuthorBox";
 import Footer from "../components/Footer/Footer";
+import Loading from "../components/Loader/Loading";
 
 const page = () => {
   return (
@@ -15,7 +18,9 @@ const page = () => {
         <HeroSection/>
       </div>
       <div className=" pb-24 lg:pb-28 2xl:pl-10 xl:pr-0 xl:max-w-none">
-        <SectionGridHasMap />
+        <Suspense fallback={<Loading />}>
+          <SectionGridHasMap />
+        </Suspense>
       </div>
       <div className="container relative mb-24 space-y-24 lg:space-y-28 lg:mb-28">
         <Subscribe />
