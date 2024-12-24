7cad56f2386a2ab26e46c532e0e1d775430bdbca
Hamza <Hamzabashir02001@gmail.com>
2024-12-24 22:13:28 +0500

    fixed add space in textarea

--- frontend/src/app/Shared/Textarea.js
+++ frontend/src/app/Shared/Textarea.js
@@ -6,6 +6,7 @@ const Textarea = React.forwardRef(({ className = "", children, ...args }, ref) =
   return (
     <textarea
       ref={ref}
+      style={{ padding: "6px" }}
       className={`block w-full text-sm rounded-2xl border-2 border-neutral-200 focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 bg-white ${className}`}
       rows={4}
       {...args}
